aff1496c18ce088f292ef7a49aec4161da71b7ad
Kazuhito Hokamura <k.hokamura@gmail.com>
2016-02-10 00:16:48 +0900

    Build frontend enviroment

--- server.js
+++ server.js
@@ -9,5 +9,8 @@ let schema = require('./schema');
 let app = express();
 
 app.use('/graphql', graphqlHTTP({ schema: schema, pretty: true }));
+app.use(express.static('public'));
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('listen: http://localhost:3000');
+});
